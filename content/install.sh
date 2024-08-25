37275532ede40a0f846d775ece8775bb3531181a
sd14vip <139548950+sd14vip@users.noreply.github.com>
2024-08-26 06:20:10 +0800

    Update install.sh

--- content/install.sh
+++ content/install.sh
@@ -4,7 +4,7 @@ set -e
 DIR_TMP="$(mktemp -d)"
 
 # Install alist
-wget -O - https://github.com/alist-org/alist/releases/latest/download/alist-linux-musl-amd64.tar.gz | tar -zxf - -C ${DIR_TMP}
+wget -O - https://github.com/alist-org/alist/releases/download/beta/alist-linux-musl-amd64.tar.gz | tar -zxf - -C ${DIR_TMP}
 install -m 755 ${DIR_TMP}/alist /usr/bin/
 
 # Install Aria2
